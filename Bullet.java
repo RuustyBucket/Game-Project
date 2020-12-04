4ab3ea541812beb56d5537558a6c9fbbf0948136
WSQD3Chimp <70527945+WSQD3Chimp@users.noreply.github.com>
2020-12-03 22:15:04 -0500

    Bullet thing

--- Bullet.java
+++ Bullet.java
@@ -6,13 +6,19 @@ import greenfoot.*;
 /**
  * 
  */
-public class Bullet extends Actor
+public class Bullet extends Kangaroo
 {
-
+    public Point2D posistion;
+    public Vector2D velocity;
+    public Vector2D acceleration;
+    
     /**
      * Act - do whatever the Bullet wants to do. This method is called whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
+        position = null;
+        velocity = new Vector2D(0.0,0.0);
+        
     }
 }
